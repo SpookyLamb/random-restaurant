336f1a693cbec20306321e2ece44df7393cd1f04
SpookyLamb <turtleytyrant@gmail.com>
2024-04-24 14:27:42 -0400

    Menu items can now display with the API!

--- src/RestMenu.jsx
+++ src/RestMenu.jsx
@@ -2,25 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
+import { useState } from "react";
+import { useEffect } from 'react';
 
 const row_classes = "d-flex justify-content-center text-center"
-
-async function GetMenuItems() {
-    axios.get("https://www.jsonkeeper.com/b/MDXW")
-        .then(function (response) {
-            // handle success
-            console.log(response.data);
-            return response.data; //our array of objects
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-            console.log("Executed Axios API Request!")
-        });
-}
 
 function Category(props) {
     const cat_name = props.category
@@ -38,8 +23,10 @@ function Item(props) {
     const price = props.price
     const description = props.description
 
+    const item_classes = row_classes + " p-3"
+
     return (
-        <Row className={row_classes}>
+        <Row className={item_classes}>
             <Col>
                 <Row className={row_classes}>
                     {name} - {type} . . . {price}
@@ -55,23 +42,56 @@ function Item(props) {
 function RestMenu() {
     //needs to construct the menu dynamically from the API call
 
-    function processItems() {
-        const ourMenu = GetMenuItems()
-        let menuItems = []
+    let [menuItems, setItems] = useState([])
+    let [processedItems, setProcessedItems] = useState([])
 
-        for (let i = 0; i < ourMenu.length; i++) {
-            menuItems.push(
-                <Item name="Food" type="American" price="$1.00" description="Made with pure human souls!"></Item>
+    function GetMenuItems() {
+        axios.get("https://www.jsonkeeper.com/b/MDXW")
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                setItems(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+                console.log("Executed Axios API Request!")
+            });
+    }
+
+    function processItems() {
+        let newItems = []
+
+        for (let i = 0; i < menuItems.length; i++) {
+            const name = menuItems[i].title
+            const type = menuItems[i].cuisine_type
+            const price = menuItems[i].price
+            const description = menuItems[i].description
+
+            newItems.push(
+                <Item name={name} type={type} price={price} description={description}></Item>
             )
         }
-        return menuItems
+
+        setProcessedItems(newItems)
     }
+
+    useEffect((() => {
+        GetMenuItems()
+    }), [])
+
+    useEffect((() => {
+        processItems()
+    }), [menuItems])
 
     return (
         <Container className="p-4">
             <Col className="col-8 mx-auto border p-3">
                 <Category category="CATEGORY"/>
-                {processItems().map((new_item) => {
+                {processedItems.map((new_item) => {
                     return new_item
                 })}
             </Col>
