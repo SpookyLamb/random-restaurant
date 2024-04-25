ffd4869ecbf2f0ab76a4e2214c827820d15c4c7d
SpookyLamb <turtleytyrant@gmail.com>
2024-04-25 15:37:42 -0400

    Tweaks.

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,11 +9,11 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-danger'/>
         <Navbar.Collapse>
           <Nav className="mx-auto">
-            <Navbar.Brand className="rubik-mono" href="#home">| THE SUMMER DEMON |</Navbar.Brand>
-            <Nav.Link className="rubik-mono" href="#link">Home</Nav.Link>
-            <Nav.Link className="rubik-mono" href="#link">About</Nav.Link>
-            <Nav.Link className="rubik-mono" href="#link">Menu</Nav.Link>
-            <Nav.Link className="rubik-mono" href="#link">Contact</Nav.Link>
+            <Navbar.Brand className="rubik-mono navlink" href="#home">| THE SUMMER DEMON |</Navbar.Brand>
+            <Nav.Link className="rubik-mono navlink" href="#link">Home</Nav.Link>
+            <Nav.Link className="rubik-mono navlink" href="#link">About</Nav.Link>
+            <Nav.Link className="rubik-mono navlink" href="#link">Menu</Nav.Link>
+            <Nav.Link className="rubik-mono navlink" href="#link">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
