2fb1ea93b6ebcfae8020d1712f0bccafaf31b628
SpookyLamb <turtleytyrant@gmail.com>
2024-05-24 08:56:01 -0400

    Fixed navbar issue.

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
           <Nav className="mx-auto">
             <Navbar.Brand className="rubik-mono navlink" href="#home">| THE SUMMER DEMON |</Navbar.Brand>
             <Nav.Link className="rubik-mono navlink" href="#link">About</Nav.Link>
-            <Nav.Link className="rubik-mono navlink" href="#link">Menu</Nav.Link>
+            <Link className="rubik-mono navlink nav-link" to="/">Menu</Link>
             <Link className="rubik-mono navlink nav-link" to="/order">Order</Link>
             <Nav.Link className="rubik-mono navlink" href="#link">Contact</Nav.Link>
           </Nav>
